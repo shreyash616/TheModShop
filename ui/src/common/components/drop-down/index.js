1a0d91c5f9cd5d12566faeaf0ae495f63174485e
shreyash616 <shreyashpadhi101@gmail.com>
2021-02-02 21:41:21 +0530

    added custom dropdown component

--- ui/src/common/components/drop-down/index.js
+++ ui/src/common/components/drop-down/index.js
@@ -1,20 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styleVars from '../../global-styles/styles'
 import {    
     StyledSelectWrapper,
-    StyledSelect,
-    StyledOption
+    StyledSelect, 
+    ListItemWrapper,   
+    ListItem,
+    RowWrapper,
+    ArrowButton,
+    Label
 } from './styles'
 
 const Dropdown = (props) => {
 
+    const [isDropdownActive, setIsDropdownActive] = useState(false)
+    const [dropdownValue, setDropdownValue] = useState('')
+
+    const triggerDropDown = (e) => {                        
+        [13, undefined].includes(e.charCode) && setIsDropdownActive(!isDropdownActive)        
+    }
+
+    const searchAndSetDropdownValue = (value) => {
+        value = String(value)[String(value).length - 1]        
+        if(props.items.length > 0) {
+            for(let i = 0; i<props.items.length; i++){
+                if(String(props.items[i]).includes(value)){
+                    console.log(props.items[i])
+                    setDropdownValue(props.items[i])
+                    break;
+                }
+            }
+        }
+    }
+
+    const handleListClick = (e, value) => {
+        if([13, undefined].includes(e.charCode)){
+            setDropdownValue(value)
+            setIsDropdownActive(false)
+        }
+    }
+
     return <StyledSelectWrapper>
+        <Label value={dropdownValue} htmlFor={`${props.id}-dropdown`}>{props.label}</Label>
+        <RowWrapper>
         <StyledSelect
-            id={props.id}
-        >
-            {props.items.map((item, i) => {
-                return <StyledOption value={item} key={`item-${i+1}`}>{item}</StyledOption>
-            })}
-        </StyledSelect>
+            id={`${props.id}-dropdown`}
+            aria-label={props.ariaLabel}
+            aria-haspopup='listbox'
+            aria-expanded='false'
+            onClick={e => triggerDropDown(e)}
+            onKeyPress={e => triggerDropDown(e)}
+            value={dropdownValue}
+            onChange={(e) => searchAndSetDropdownValue(e.target.value)}            
+        />
+        <ArrowButton onClick={e => triggerDropDown(e)} onKeyPress={e => triggerDropDown(e)} isDropdownActive={isDropdownActive} width="24" height="24" viewBox="0 0 24 24"><path role='button' tabIndex={0} onClick={e => triggerDropDown(e)} onKeyPress={e => triggerDropDown(e)} d={styleVars.icons.downArrow} fill='white'/></ArrowButton>
+        </RowWrapper>
+        <ListItemWrapper>
+        {isDropdownActive && props.items.map((item, i) => {
+            return <ListItem key={`${props.id}-item-${i+1}`} onClick={(e) => handleListClick(e, item)}>{item}</ListItem>
+        })}        
+        </ListItemWrapper>
     </StyledSelectWrapper>
 }
 
--- ui/src/common/components/drop-down/styles.js
+++ ui/src/common/components/drop-down/styles.js
@@ -5,7 +5,22 @@ export const StyledSelectWrapper = styled.div`
     display: flex;
     flex-direction: column;
 `
-export const StyledSelect = styled.select`
+export const RowWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    position: relative;
+`
+export const Label = styled.label`    
+    margin-bottom: ${styleVars.spacing.spacing0};
+    margin-left: 1px;  
+    color: solid ${styleVars.colors.white};   
+    @media ${styleVars.breakpoints.tabletLandscape}, ${styleVars.breakpoints.tabletPortrait} {
+        margin-left: ${styleVars.spacing.spacing2};
+    }
+    transform: ${props => !props.value && 'translate(0px, 32px)'};    
+    transition: all 0.5s;
+`
+export const StyledSelect = styled.input`
     display: block;
     outline: none;
     width: 100%;
@@ -20,12 +35,45 @@ export const StyledSelect = styled.select`
     &:focus {
         border-bottom: ${styleVars.spacing.spacing2} solid ${props => props.error?styleVars.colors.red:styleVars.colors.oceanBlue};
     }
-    option {
-        background: black;
-        padding-top: ${styleVars.spacing.spacing50};
-        padding-bottom: ${styleVars.spacing.spacing50};
+    &:hover {
+        border-bottom: ${styleVars.spacing.spacing2} solid ${props => props.error?styleVars.colors.red:styleVars.colors.oceanBlue};        
+        cursor: pointer;
     }
 `
-
-export const StyledOption = styled.option`
+export const ArrowButton = styled.svg`    
+    position: absolute;
+    padding: ${styleVars.spacing.spacing4};
+    display: block;    
+    right: -${styleVars.spacing.spacing0};
+    top: ${styleVars.spacing.spacing14};
+    transform: ${props => props.isDropdownActive ? 'scale(2) rotate(180deg) translate(4px, 2px)' : 'scale(2) rotate(0deg)'};
+    &:hover {        
+        cursor: pointer;
+    }
+    transition: transform 0.2s linear;    
+`
+export const ListItemWrapper = styled.div`
+    position: relative;
+    z-index: 1000;
+`
+export const ListItem = styled.li`
+    display: block;
+    outline: none;
+    width: 100%;
+    background: none;
+    font-size: ${styleVars.spacing.spacing16};
+    font-family: ${styleVars.fonts.globalFont};
+    color: ${styleVars.colors.white};
+    padding: ${styleVars.spacing.spacing12};    
+    border: none;    
+    border-bottom: ${styleVars.spacing.spacing2} solid ${props => props.error?styleVars.colors.red:styleVars.colors.white};
+    &:focus {
+        border-bottom: ${styleVars.spacing.spacing2} solid ${props => props.error?styleVars.colors.red:styleVars.colors.oceanBlue};
+        background-color: rgba(255, 255, 255, 0.3);
+    }
+    &:hover {
+        border-bottom: ${styleVars.spacing.spacing2} solid ${props => props.error?styleVars.colors.red:styleVars.colors.oceanBlue};
+        background-color: rgba(255, 255, 255, 0.3);
+        cursor: pointer;
+    }
 `
